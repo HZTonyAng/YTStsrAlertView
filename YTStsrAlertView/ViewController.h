3c22530e42b35c3579406c84586b0726fd953896
TonyAng <13323650885@163.com>
2018-04-20 15:07:57 +0800

    Initial Commit

---
+++ YTStsrAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTStsrAlertView
+//
+//  Created by TonyAng on 2018/4/20.
+//  Copyright © 2018年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YTStsrAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YTStsrAlertView
+//
+//  Created by TonyAng on 2018/4/20.
+//  Copyright © 2018年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
